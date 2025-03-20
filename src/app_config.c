f46073af4258ed4ae48ba7c1d2b7794a05a09216
Benedikt Ackermann <benacke@gmail.com>
2025-03-20 16:48:42 +0100

    kinda working

--- src/app_config.c
+++ src/app_config.c
@@ -3,12 +3,12 @@
 /* Provide default values for your global config. */
 light_config_t g_light_config = {
     .offset1         = 0,
-    .offset2         = 0,
+    .offset2         = 50,
     .levelMin        = 5,
     .levelMax        = 254,
-    .onTime          = 100,
-    .offTime         = 100,
-    .transitionTime  = 200,
+    .onTime          = 1,
+    .offTime         = 1,
+    .transitionTime  = 20,
     .gammaVal        = 2.2,
     .dimmingMode     = DIMMING_MODE_LINEAR
 };
